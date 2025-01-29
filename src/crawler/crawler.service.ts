21a2d06d1cd1d987fcf6ff30a57ba15ed213ab1b
Yousaf Chaudhary <yousafch360@gmail.com>
2025-01-29 17:29:50 +0500

    update : removed unneccessary code

--- src/crawler/crawler.service.ts
+++ src/crawler/crawler.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import puppeteer from 'puppeteer';
 import { SupabaseService } from '../supabase/supabase.service';
-@Injectable()
 
+interface ScrapedItem {
+  articleDate: Date;
+  link: string;
+  title: string;
+  description: string;
+  pageText: string;
+  is_processed: boolean;
+  id: string;
+}
+
+@Injectable()
 export class CrawlerService {
   constructor(private readonly supabaseService: SupabaseService){}
   async scrape(cutoffDate: Date) {
@@ -18,15 +28,50 @@ export class CrawlerService {
       ],
       timeout: 60000 
     });
-    // const supabase = await this.supabaseService.getClient();
-    // const { data, error } = await supabase.from('substack').select('id,link');
-    // for (const item of data){
-    //   console.log('id : ' , item.id);
-    //   console.log('link : ' , item.link);
-    // }
-    // return data;
+
+
     try {
-      const page = await browser.newPage();
+      const supabase = await this.supabaseService.getClient();
+      const { data, error } = await supabase.from('substack').select('id,link');
+      const results = new Set();
+
+      for (const item of data){
+        const temp = await this.scrapePage(browser,item.link,item.id,results,cutoffDate);
+      }
+      await browser.close();
+      const resultsArray = Array.from(results);
+      console.log('Results to save:', resultsArray);
+
+      for (const item of resultsArray as ScrapedItem[]) {
+        const { data, error } = await supabase.from('details').insert({
+          dated_at: item.articleDate,
+          link: item.link,
+          title: item.title,
+          description: item.description,
+          content: item.pageText,
+          is_processed: true,
+          reference_id: item.id
+        });
+
+        if (error) {
+          console.error('Supabase insert error:', error);
+        } else {
+          console.log('Saved item:', data);
+        }
+      }
+      console.log('the end!')
+      return resultsArray;
+
+    } catch (error) {
+      console.error('Scraping error:', error);
+      throw error;
+    }
+
+  }
+
+  private async scrapePage(browser, link,id,results, cutoffDate){
+    const page = await browser.newPage();
+    try{
       await page.setDefaultNavigationTimeout(60000);
       await page.setDefaultTimeout(60000);
       
@@ -38,32 +83,30 @@ export class CrawlerService {
           request.continue();
         }
       });
-
-      await page.goto('https://freddiedeboer.substack.com/archive', {
+      await page.goto(link, {
         waitUntil: 'networkidle2',
         timeout: 60000
       });
       
       await page.waitForSelector('.container-Qnseki');
-
-      const results = new Set();
+  
       let lastHeight = await page.evaluate('document.body.scrollHeight');
-
+  
       while (true) {
         const containers = await page.$$('.container-Qnseki');
-
+  
         for (const container of containers) {
           const dateElement = await container.$('time');
           const dateStr = await dateElement?.evaluate((el) =>
             el.getAttribute('datetime'),
           );
           const articleDate = dateStr ? new Date(dateStr) : null;
-
+  
           if (articleDate && articleDate.getTime() < cutoffDate.getTime()) {
-            await browser.close();
+            // await browser.close();
             return Array.from(results);
           }
-
+  
           const title = await container.$eval(
             'a[data-testid="post-preview-title"]',
             (el) => el.textContent.trim(),
@@ -76,97 +119,51 @@ export class CrawlerService {
             'a[data-testid="post-preview-title"]',
             (el) => el.href,
           );
-
+  
           const articlePage = await browser.newPage();
           await articlePage.goto(link);
           await articlePage.waitForSelector('.available-content');
-
+  
           const pageText = await articlePage.$$eval(
-            '.available-content',
+            'div.available-content',
             (elements) => elements.map((el) => el.textContent.trim()),
           );
-
+  
           const imageUrls = await articlePage.$$eval(
             '.available-content img',
             (imgs) => imgs.map((img) => img.src),
           );
-
+  
           await articlePage.close();
-
+          console.log('info : ',title,description,link,articleDate,pageText,imageUrls,id);
           results.add({
             title,
             description,
             link,
             articleDate,
             pageText,
+
             imageUrls,
+            id
           });
         }
-
+  
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
         await new Promise((resolve) => setTimeout(resolve, 2000));
-
+  
         const newHeight = await page.evaluate('document.body.scrollHeight');
         if (newHeight === lastHeight) {
           break;
         }
         lastHeight = newHeight;
       }
-
-      await browser.close();
-      return Array.from(results);
-    } catch (error) {
+    }
+    catch(error){
       console.error('Scraping error:', error);
       throw error;
+    }finally{
+      await page.close();
     }
   }
-  // await page.locator('.container-Qnseki').click();
 
-  // const results = await Promise.all(
-  //     containers.map(async container =>{
-  //         console.log(container);
-  //         // await container.click();
-  //         // await page.waitForSelector('.available-content');
-  //         // const elements = await page.$$('.available-content');
-  //         // const pageText = await Promise.all(
-  //         //     elements.map(element => element.evaluate(el => el.textContent))
-  //         // );
-  //     })
-
-  // );
-
-  // await page.waitForSelector('.available-content');
-
-  // const elements = await page.$$('.available-content');
-  // const pageText = await Promise.all(
-  //     elements.map(element => element.evaluate(el => el.textContent))
-  // );
-
-  // await page.waitForSelector('.available-content img');
-  // const images = await page.$$('.available-content img');
-  // const imageUrls = await Promise.all(
-  //     images.map(img => img.evaluate(el => el.src))
-  // );
-
-  // console.log(pageText);
-  // return results;
-
-  //     // await page.locator('.homepage-nav-search-bar').fill('ai');
-  //     await page.locator('.homepage-nav-search-bar').click();
-  //     const searchInput = page.locator('input[name="search-dialog-input"]');
-  //     await searchInput.fill('ai');
-  //         // Click on the search suggestion containing the text 'ai'
-  // await page.evaluate(() => {
-  //     const elements = document.querySelectorAll('div.pencraft span.weight-bold-DmI9lw');
-  //     for (const element of elements) {
-  //       if (element.textContent?.trim() === 'ai') {
-  //         (element as HTMLElement).click();
-  //         break;
-  //       }
-  //     }
-  //   });
-  //   await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-  // const content = await page.content();
-  // }
 }
